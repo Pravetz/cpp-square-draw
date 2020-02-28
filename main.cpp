95b78d423d72f130259a7b6fb0c6d9d4498041a6
Pravetz <46265909+Pravetz@users.noreply.github.com>
2020-02-28 10:00:15 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,36 @@
 #include <time.h>
 
 using namespace std;
-int w, h, lw;
+int w, h, lw, ttp;
+
+void drawtriangle()
+{
+	int CASE;
+	srand(time(0));
+	CASE = rand() % 2 + 1;
+	if(CASE==1){
+		for(int i=0; i<=7; i++){
+			for(int j=1; j<=i; j++){
+				cout<<"*";
+				}
+			cout<<""<<endl;
+			}
+		
+		}
+	if(CASE==2){
+		for(int i=0; i<7; i++){
+			for(int j=0; j<i; j++){
+				cout<<" ";
+				}
+			for(int l=7; l>=i; l--){
+				cout<<"*";
+				}
+			cout<<""<<endl;
+			}
+		
+		}		
+}
+	
 
 void drawline()
 {
@@ -22,7 +51,7 @@ void draw()
 {
 	for(int i=0; i<w+1; i++) //width
 		cout<<"*";
-		cout<<""<<endl;
+		cout<<endl;
 		
 	for(int i=0; i<h; i++){		//height
 		for(int j=0; j<w; j++){
@@ -35,7 +64,7 @@ void draw()
 			
 	for(int i=0; i<w+1; i++) //also width
 		cout<<"*";
-		cout<<""<<endl;
+		cout<<endl;
 	
 }
 void geometry()
@@ -51,13 +80,19 @@ int choice()
 	cout<<"(Enter a number)Draw: "<<endl;
 	cout<<"1. Square"<<endl;
 	cout<<"2. Line"<<endl;
+	cout<<"3. Triangle"<<endl;
+	cout<<"4. Exit"<<endl;
 	cin>>c;
-	if(c!=1&&c!=2) /*automatically sets c(choice) to 2 if c is not equal to 1 or 2*/
-		c=2;
+	if(c!=1&&c!=2&&c!=3&&c!=4) /*automatically sets c(choice) to 3 if c is not equal to 1, 2, 3 or 4 */
+		c=3;
 	if(c==1)
 		return 0;
 	if(c==2)
 		return 1;
+	if(c==3)
+		return 2;
+	if(c==4)
+		return 3;
 		
 	return 0;
 }
@@ -78,5 +113,11 @@ int main()
 		cin>>lw;
 		drawline();
 	}
+	if(cdone==2){
+		drawtriangle();
+		}
+	if(cdone==3){
+		return 0;
+		}
 	
 }
