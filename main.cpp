50f57e8f01c6ce3f55bcf8735eaca7a1e8c531dc
Pravetz <46265909+Pravetz@users.noreply.github.com>
2020-04-03 10:20:24 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <time.h>
 
 using namespace std;
-int w, h, lw, ttp;
+int w, h, lw, ttp, c;
+string st="*";
 
 void drawtriangle()
 {
@@ -14,7 +15,7 @@ void drawtriangle()
 	if(CASE==1){
 		for(int i=0; i<=7; i++){
 			for(int j=1; j<=i; j++){
-				cout<<"*";
+				cout<<st;
 				}
 			cout<<""<<endl;
 			}
@@ -26,7 +27,7 @@ void drawtriangle()
 				cout<<" ";
 				}
 			for(int l=7; l>=i; l--){
-				cout<<"*";
+				cout<<st;
 				}
 			cout<<""<<endl;
 			}
@@ -42,81 +43,76 @@ void drawline()
 	CASE = rand() % 2 + 1;
 	if(CASE==1){
 		for(int i=0; i<lw+1; i++)
-			cout<<"*";}
+			cout<<st;}
 	if(CASE==2){
 		for(int i=0; i<lw+1; i++)
-			cout<<"*"<<endl;}
+			cout<<st<<endl;}
 }
 void draw()
 {
 	for(int i=0; i<w+1; i++) //width
-		cout<<"*";
+		cout<<st;
 		cout<<endl;
 		
 	for(int i=0; i<h; i++){		//height
 		for(int j=0; j<w; j++){
 			if(j==0 || j==w-1)
-			cout<<"*";
+			cout<<st;
 			cout<<" ";
 		}
 		cout<<""<<endl;
 	}
 			
 	for(int i=0; i<w+1; i++) //also width
-		cout<<"*";
+		cout<<st;
 		cout<<endl;
 	
 }
 void geometry()
 {
 	if(w==h)
-		cout<<"Square ("<<w*h<<")"<<endl;
+		cout<<"Square ("<<"S="<<""<<w*h<<")"<<endl;
 	if(w>h||w<h)
-		cout<<"Rectangle ("<<w<<"*"<<h<<"="<<w*h<<")"<<endl;
+		cout<<"Rectangle ("<<"S="<<""<<w<<"*"<<h<<"="<<w*h<<")"<<endl;
 }
 int choice()
 {
-	int c;
 	cout<<"(Enter a number)Draw: "<<endl;
 	cout<<"1. Square"<<endl;
 	cout<<"2. Line"<<endl;
 	cout<<"3. Triangle"<<endl;
-	cout<<"4. Exit"<<endl;
+	cout<<"4. Customize figure border"<<endl;
+	cout<<"5. Exit"<<endl;
 	cin>>c;
-	if(c!=1&&c!=2&&c!=3&&c!=4) /*automatically sets c(choice) to 3 if c is not equal to 1, 2, 3 or 4 */
-		c=3;
-	if(c==1)
-		return 0;
-	if(c==2)
-		return 1;
-	if(c==3)
-		return 2;
-	if(c==4)
-		return 3;
-		
+	if(c==4){
+		cout<<"Enter a symbol:";
+		cin>>st;
+		st=st[0];
+		cout<<"Draw:";
+		cin>>c;
+	}
 	return 0;
 }
 
 int main()
 {
-	int cdone=0;
-	cdone=choice();
-	if(cdone==0){
+	choice();
+	if(c==1){
 	cout<<"Enter width: ";
 	cin>>w;
 	cout<<"Enter height: ";
 	cin>>h;
 	draw();
 	geometry();}
-	if(cdone==1){
+	if(c==2){
 		cout<<"Enter line width: ";
 		cin>>lw;
 		drawline();
 	}
-	if(cdone==2){
+	if(c==3){
 		drawtriangle();
 		}
-	if(cdone==3){
+	if(c==5){
 		return 0;
 		}
 	
